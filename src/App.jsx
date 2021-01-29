67ee010706c72b5dbca87af4079785cd9d03f8cb
Jackson Cai <jcai8649@gmail.com>
2021-01-28 17:49:11 -0800

    Structure with react router

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import CoinDetailPage from './pages/CoinDetailPage';
+import CoinSummaryPage from './pages/CoinSummaryPage';
+import Header from './components/Header'
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                    <Header />
+                    <Route exact path='/' component={CoinSummaryPage}/>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
